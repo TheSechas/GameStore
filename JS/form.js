55c1fdb1a243c9e06373c756488a3c27ac00f176
Sergio González García <sergiogg20012003@gmail.com>
2025-02-26 19:01:11 +0100

    correccion js

--- JS/form.js
+++ JS/form.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const form = document.getElementById('form-contacto');
 form.addEventListener('submit', (e) => {
     e.preventDefault();
@@ -18,25 +17,4 @@ form.addEventListener('submit', (e) => {
 }
     alert('Formulario enviado correctamente (simulado)!');
     form.reset();
-=======
-const form = document.getElementById('form-contacto');
-form.addEventListener('submit', (e) => {
-    e.preventDefault();
-    const nombre = document.getElementById('nombre').value;
-    const email = document.getElementById('email').value;
-    const mensaje = document.getElementById('mensaje').value;
-
-    if (!nombre || !email || !mensaje) {
-    alert('Por favor, completa todos los campos.');
-    return;
-}
-  // Validación básica de email
-    const emailRegex = /\S+@\S+\.\S+/;
-    if (!emailRegex.test(email)) {
-    alert('Por favor, introduce un email válido.');
-    return;
-}
-    alert('Formulario enviado correctamente (simulado)!');
-    form.reset();
->>>>>>> a9e567e (Mejoras)
 });
